723f847fd00e655673bec8060f4052c78d525977
Gerardo Hernandez <gerry_hd@live.com.mx>
2021-10-27 14:09:19 -0500

    Add secure password digest to User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_20_024237) do
+ActiveRecord::Schema.define(version: 2021_10_27_190423) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_10_20_024237) do
     t.string "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
