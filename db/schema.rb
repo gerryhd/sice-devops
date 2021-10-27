2f9f7a448ac1f556e6420361fd538b6115a3b568
Gerardo Hernandez <gerry_hd@live.com.mx>
2021-10-27 16:21:20 -0500

    Add migration to add email and create courses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_27_190423) do
+ActiveRecord::Schema.define(version: 2021_10_27_194734) do
+
+  create_table "courses", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_10_27_190423) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "email"
   end
 
 end
